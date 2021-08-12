085697d43384e8a0a6783f4e23dc2d7083b95305
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-11 22:50:07 -0400

    Track location and update, WIP

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import "../_mockLocation";
+import React, { useState, useEffect, useContext } from "react";
+import { Context as LocationContext } from "../context/LocationContext";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Map from "../components/Map";
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
 
 const TrackCreateScreen = () => {
-    return (
-        <View>
-            <Text style={{fontSize: 48}}> Track Create Screen</Text>
-        </View>
-    );
-}
+  const [err, setErr] = useState(null);
+  const { addLocation } = useContext(LocationContext);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          addLocation(location);
+        }
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return (
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h2>Create a Track</Text>
+      <Map />
+      {err && <Text>Please enable location services</Text>}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
 
 export default TrackCreateScreen;
