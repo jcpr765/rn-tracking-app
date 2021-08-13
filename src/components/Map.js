599d782ae966b296d7b32bdc414980d9649c99ae
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-13 17:56:42 -0400

    Add circle indicating the tracked position

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import { Context as LocationContext } from "../context/LocationContext";
 import { StyleSheet, ActivityIndicator } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 
 const Map = () => {
+  // const { testLatitude, testLongitude } = getEnvVars();
   const {
     state: { currentLocation },
   } = useContext(LocationContext);
@@ -20,12 +21,14 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }}
-    ></MapView>
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
