aed678f3905903141a37cbd046a7e117cb829e75
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-13 22:20:10 -0400

    Automatically add token into requests for authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 import getEnvVars from "../../environment";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const { trackerApiBaseUrl } = getEnvVars();
 
-export default axios.create({
+const instance = axios.create({
   baseURL: trackerApiBaseUrl,
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
